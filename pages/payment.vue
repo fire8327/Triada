5521152d6ffbde790dae31387f7b7ec5ca1c6a16
fire8327 <fire83274@gmail.com>
2024-12-15 19:51:45 +0300

    create payment

---
+++ pages/payment.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex flex-col gap-8 w-full grow justify-center">
+        <p>Мы предлагаем удобные и гибкие способы оплаты, чтобы сотрудничество с нами было комфортным для каждого клиента. Вы можете выбрать один из следующих вариантов:</p>
+        <div class="flex flex-col gap-6 w-full">
+            <div class="w-full h-px bg-[#f9f9f9]"></div>
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold text-2xl font-Cormorant">1. Безналичный расчет</p>
+                <p class="text-gray-400">Оплата через банковский перевод на расчётный счёт компании. Реквизиты предоставляются при оформлении договора.</p>
+            </div>
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold text-2xl font-Cormorant">2. Банковские карты</p>
+                <p class="text-gray-400">Мы принимаем платежи с карт Visa, Mastercard и МИР. Вы можете оплатить услуги через наш сайт или терминал в офисе.</p>
+            </div>
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold text-2xl font-Cormorant">3. Онлайн-платежи</p>
+                <p class="text-gray-400">Оплата с помощью популярных платёжных систем, таких как ЮKassa, PayPal или Stripe. Все платежи защищены и обрабатываются через безопасные шлюзы.</p>
+            </div>
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold text-2xl font-Cormorant">4. Наличный расчет</p>
+                <p class="text-gray-400">Доступен в нашем офисе при заключении договора. Вы получите подтверждение оплаты в виде кассового чека.</p>
+            </div>
+            <div class="flex flex-col gap-2">
+                <p class="font-semibold text-2xl font-Cormorant">5. Кредитование или рассрочка</p>
+                <p class="text-gray-400">Для крупных проектов мы предоставляем возможность оплаты в рассрочку. Условия согласовываются индивидуально.</p>
+            </div>
+            <div class="w-full h-px bg-[#f9f9f9]"></div>
+        </div>
+        <p class="text-center text-2xl"><span class="animate-text text-transparent !bg-clip-text">Triada</span> — удобство и доверие на каждом этапе сотрудничества.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
