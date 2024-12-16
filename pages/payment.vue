5b88205cd60807de26504d8dbaef26e5eb958046
fire8327 <fire83274@gmail.com>
2024-12-16 12:17:04 +0300

    upd payment

--- pages/payment.vue
+++ pages/payment.vue
@@ -4,24 +4,16 @@
         <div class="flex flex-col gap-6 w-full">
             <div class="w-full h-px bg-[#f9f9f9]"></div>
             <div class="flex flex-col gap-2">
-                <p class="font-semibold text-2xl font-Cormorant">1. Безналичный расчет</p>
-                <p class="text-gray-400">Оплата через банковский перевод на расчётный счёт компании. Реквизиты предоставляются при оформлении договора.</p>
+                <p class="font-semibold text-2xl font-Cormorant">1. Перевод по номеру телефона</p>
+                <p class="text-gray-400">Вы можете перевести оплату напрямую на номер телефона нашего менеджера. Реквизиты доступны при заключении договора.</p>
             </div>
             <div class="flex flex-col gap-2">
-                <p class="font-semibold text-2xl font-Cormorant">2. Банковские карты</p>
-                <p class="text-gray-400">Мы принимаем платежи с карт Visa, Mastercard и МИР. Вы можете оплатить услуги через наш сайт или терминал в офисе.</p>
+                <p class="font-semibold text-2xl font-Cormorant">2. Оплата по QR-коду</p>
+                <p class="text-gray-400">Удобный способ перевода через мобильное приложение вашего банка. QR-код предоставляется в договоре или по запросу.</p>
             </div>
             <div class="flex flex-col gap-2">
-                <p class="font-semibold text-2xl font-Cormorant">3. Онлайн-платежи</p>
-                <p class="text-gray-400">Оплата с помощью популярных платёжных систем, таких как ЮKassa, PayPal или Stripe. Все платежи защищены и обрабатываются через безопасные шлюзы.</p>
-            </div>
-            <div class="flex flex-col gap-2">
-                <p class="font-semibold text-2xl font-Cormorant">4. Наличный расчет</p>
-                <p class="text-gray-400">Доступен в нашем офисе при заключении договора. Вы получите подтверждение оплаты в виде кассового чека.</p>
-            </div>
-            <div class="flex flex-col gap-2">
-                <p class="font-semibold text-2xl font-Cormorant">5. Кредитование или рассрочка</p>
-                <p class="text-gray-400">Для крупных проектов мы предоставляем возможность оплаты в рассрочку. Условия согласовываются индивидуально.</p>
+                <p class="font-semibold text-2xl font-Cormorant">3. Перевод на банковский счёт</p>
+                <p class="text-gray-400">Оплатите услуги вручную, используя реквизиты компании. Мы предоставим все необходимые данные для перевода.</p>
             </div>
             <div class="w-full h-px bg-[#f9f9f9]"></div>
         </div>
