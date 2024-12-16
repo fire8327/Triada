5814acaacc54da182a3233ae6abdfbccca27c1d5
fire8327 <fire83274@gmail.com>
2024-12-16 22:16:50 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,26 @@
     <div class="flex flex-col gap-6">
         <p class="mainHeading">Почему вам стоит выбрать именно нас?</p>
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            <div class="flex flex-col p-4 border border-[#f9f9f9]/30 rounded-xl gap-4 transition-all duration-500 hover:bg-[#191919]">
+            <div class="flex flex-col p-4 rounded-xl gap-4 transition-all duration-500 bg-[#252525] border border-white/10 shadow-[0px_0px_13px_-7px_white]">
                 <div class="flex items-center justify-between gap-4">
-                    <p class="text-2xl">Индивидуальный подход</p>
+                    <p class="font-medium">Индивидуальный подход</p>
                     <Icon class="text-3xl text-[#673ab7]" name="material-symbols:person"/>
                 </div>
-                <p class="grow">Мы понимаем, что каждый бизнес уникален. Поэтому наши стратегии разрабатываются с учётом ваших целей, аудитории и особенностей рынка.</p>
+                <p class="grow text-sm text-gray-400">Мы понимаем, что каждый бизнес уникален. Поэтому наши стратегии разрабатываются с учётом ваших целей, аудитории и особенностей рынка.</p>
             </div>
-            <div class="flex flex-col p-4 border border-[#f9f9f9]/30 rounded-xl gap-4 transition-all duration-500 hover:bg-[#191919]">
+            <div class="flex flex-col p-4 rounded-xl gap-4 transition-all duration-500 bg-[#252525] border border-white/10 shadow-[0px_0px_13px_-7px_white]">
                 <div class="flex items-center justify-between gap-4">
-                    <p class="text-2xl">Комплексные решения</p>
+                    <p class="font-medium">Комплексные решения</p>
                     <Icon class="text-3xl text-[#673ab7]" name="carbon:decision-tree"/>
                 </div>
-                <p class="grow">От разработки брендинга до запуска масштабных рекламных кампаний — мы предлагаем полный спектр услуг, чтобы вам не приходилось искать подрядчиков в разных местах.</p>
+                <p class="grow text-sm text-gray-400">От разработки брендинга до запуска масштабных рекламных кампаний — мы предлагаем полный спектр услуг, чтобы вам не приходилось искать подрядчиков в разных местах.</p>
             </div>
-            <div class="flex flex-col p-4 border border-[#f9f9f9]/30 rounded-xl gap-4 transition-all duration-500 hover:bg-[#191919]">
+            <div class="flex flex-col p-4 rounded-xl gap-4 transition-all duration-500 bg-[#252525] border border-white/10 shadow-[0px_0px_13px_-7px_white]">
                 <div class="flex items-center justify-between gap-4">
-                    <p class="text-2xl">Прозрачность</p>
+                    <p class="font-medium">Прозрачность</p>
                     <Icon class="text-3xl text-[#673ab7]" name="mdi:circle-opacity"/>
                 </div>
-                <p class="grow">Мы регулярно предоставляем отчёты о проделанной работе и достигнутых результатах. Ваша инвестиция в рекламу работает эффективно.</p>
+                <p class="grow text-sm text-gray-400">Мы регулярно предоставляем отчёты о проделанной работе и достигнутых результатах. Ваша инвестиция в рекламу работает эффективно.</p>
             </div>
         </div>
     </div>
@@ -46,6 +46,33 @@
             </div>
         </div>
         <NuxtLink to="/about" class="px-4 py-2 border border-[#673ab7] text-[#673ab7] rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-[#673ab7] self-end">Подробнее</NuxtLink>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="mainHeading">Примеры услуг</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <NuxtLink to="/" class="flex flex-col gap-4 bg-[#252525] rounded-xl p-6 border border-white/10 shadow-[0px_0px_13px_-7px_white] transition-all duration-500 hover:shadow-none">
+                <div class="w-16 h-16 flex items-center justify-center p-2 bg-[#673ab7] rounded-full">
+                    <Icon class="text-3xl" name="material-symbols:brand-family"/>
+                </div>
+                <span class="font-medium">Разработка бренда</span>
+                <span class="text-sm text-gray-400">Создание логотипа, фирменного стиля и брендбука для вашего бизнеса.</span>
+            </NuxtLink>
+            <NuxtLink to="/" class="flex flex-col gap-4 bg-[#252525] rounded-xl p-6 border border-white/10 shadow-[0px_0px_13px_-7px_white] transition-all duration-500 hover:shadow-none">
+                <div class="w-16 h-16 flex items-center justify-center p-2 bg-[#673ab7] rounded-full">
+                    <Icon class="text-3xl" name="material-symbols:brand-family"/>
+                </div>
+                <span class="font-medium">Разработка бренда</span>
+                <span class="text-sm text-gray-400">Создание логотипа, фирменного стиля и брендбука для вашего бизнеса.</span>
+            </NuxtLink>
+            <NuxtLink to="/" class="flex flex-col gap-4 bg-[#252525] rounded-xl p-6 border border-white/10 shadow-[0px_0px_13px_-7px_white] transition-all duration-500 hover:shadow-none">
+                <div class="w-16 h-16 flex items-center justify-center p-2 bg-[#673ab7] rounded-full">
+                    <Icon class="text-3xl" name="material-symbols:brand-family"/>
+                </div>
+                <span class="font-medium">Разработка бренда</span>
+                <span class="text-sm text-gray-400">Создание логотипа, фирменного стиля и брендбука для вашего бизнеса.</span>
+            </NuxtLink>
+        </div>
+        <NuxtLink to="/services" class="px-4 py-2 border border-[#673ab7] text-[#673ab7] rounded-full w-[160px] text-center transition-all duration-500 hover:text-white hover:bg-[#673ab7] self-end">Подробнее</NuxtLink>
     </div>
     <div class="flex flex-col gap-6">
         <p class="mainHeading">Наши партнёры</p>
