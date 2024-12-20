db013377f916ed04ac073416038ee07c015cbf34
fire8327 <fire83274@gmail.com>
2024-12-20 14:26:33 +0300

    upd slider

--- pages/reviews.vue
+++ pages/reviews.vue
@@ -1,30 +1,16 @@
 <template>
-    <!-- <Swiper
-      class="w-full"
-      :space-between="20"
-      :slides-per-view="1"
-      :loop="true"
-      :autoplay="{ delay: 3500 }"
-      :pagination="{ clickable: true }"
-      modules="[Autoplay, Pagination]"
-    >
-      <SwiperSlide class="flex flex-col gap-4 p-4 rounded-xl border border-white/10 bg-[#252525] w-full">
-        <p class="text-[#673ab7] font-semibold">Иванов Иван</p>
-        <p class="font-Cormorant">Услуга: Разработка веб-сайтов</p>
-        <p class="text-gray-400 text-lg">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        </p>
-      </SwiperSlide>
-      <SwiperSlide class="flex flex-col gap-4 p-4 rounded-xl border border-white/10 bg-[#252525] w-full">
-        <p class="text-[#673ab7] font-semibold">Иванов Иван</p>
-        <p class="font-Cormorant">Услуга: Разработка веб-сайтов</p>
-        <p class="text-gray-400 text-lg">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        </p>
-      </SwiperSlide>
-    </Swiper> -->
+  <!-- Слайдер -->
+    <ClientOnly>
+      <swiper-container ref="containerRef" class="w-full" :loop="true" :autoplay="{ delay: 2500 }">
+        <swiper-slide class="flex flex-col gap-4 p-4 rounded-xl border border-white/10 bg-[#252525] w-full" v-for="review in reviews">
+          <p class="text-[#673ab7] font-semibold">{{ review.users.surname }} {{ review.users.name }}</p>
+          <p class="font-Cormorant">Услуга: {{ review.services.title }}</p>
+          <p class="text-gray-400 text-lg">{{ review.text }}</p>
+        </swiper-slide>
+      </swiper-container>
+    </ClientOnly>
     <!-- Форма добавления отзыва (если пользователь вошёл в аккаунт) -->
-    <FormKit @submit="addReview" v-if="authenticated" type="form" :actions="false" messages-class="hidden" form-class="flex flex-col gap-6">
+    <FormKit @submit="addReview" v-if="authenticated && role == 'user'" type="form" :actions="false" messages-class="hidden" form-class="flex flex-col gap-6">
         <FormKit v-model="reviewForm.id" validation="required" messages-class="text-[#E9556D] font-Cormorant" type="select" :options="serviceOptions" placeholder="Выберите услугу" name="Услуга" outer-class="w-full" input-class="focus:outline-none px-4 py-2 bg-transparent rounded-xl border border-white/15 w-full transition-all duration-500 focus:border-white focus:bg-[#191919]"/>
         <FormKit v-model="reviewForm.text" validation="required" messages-class="text-[#E9556D] font-Cormorant" type="textarea" placeholder="Текст отзыва" name="Текст отзыва" outer-class="w-full" input-class="focus:outline-none px-4 py-2 bg-transparent rounded-xl border border-white/15 w-full transition-all duration-500 focus:border-white focus:bg-[#191919]"/>
         <button type="submit" class="px-4 py-2 border border-[#673ab7] bg-[#673ab7] text-white rounded-full w-[160px] text-center transition-all duration-500 hover:text-[#673ab7] hover:bg-transparent mx-auto">Отправить</button>
@@ -87,9 +73,41 @@
 
 
   /* проверка входа */
-  const { authenticated, id } = storeToRefs(useUserStore())
+  const { authenticated, id, role } = storeToRefs(useUserStore())
 
 
   /* создание сообщений */
   const { showMessage } = useMessagesStore()
+
+
+  /* слайдер */
+  const containerRef = ref(null)
+  const swiper = useSwiper(containerRef, {
+    loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,        
+        },
+        768: {
+          slidesPerView: 2,         
+        },
+        1024: {
+          slidesPerView: 3,         
+        },
+        1280: {
+          slidesPerView: 4,         
+        }
+    }
+  })
+
+  onMounted(() => {
+    console.log(swiper.instance)
+  })
+
+
+  /* получение отзывов */
+  const { data:reviews, error:reviewsError } = await supabase
+  .from('reviews')
+  .select('*, users(*), services(*)') 
 </script>
