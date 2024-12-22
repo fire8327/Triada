80c6a2f30bf9f40d5489ff8114c95bc99ca60d7e
fire8327 <fire83274@gmail.com>
2024-12-22 13:58:34 +0300

    upd service page

--- pages/services/service-[id].vue
+++ pages/services/service-[id].vue
@@ -4,6 +4,7 @@
         <NuxtLink to="/services" class="self-start px-4 py-2 rounded-xl border border-white/10 shadow-[0px_0px_13px_-7px_white] bg-[#191919]">Вернуться назад</NuxtLink>
         <p class="mainHeading">Услуга: {{ data[0].title }}</p>
         <p class="text-lg text-gray-400 max-w-2xl">{{ data[0].shortDesc }}</p>
+        <img src="/public/images/portfolio/1.jpg" alt="" class="rounded-xl aspect-video object-cover w-full lg:w-2/3">
     </div>
     
     <!-- Преимущества -->
@@ -11,19 +12,36 @@
         <p class="mainHeading text-center">Преимущества</p>
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <div v-for="advantage in data[0].advantages" class="bg-[#252525] shadow-[0px_0px_13px_-7px_white] rounded-xl p-6 flex flex-col gap-6 items-center text-center border border-white/10">
-                <p class="text-[#673ab7] text-2xl font-semibold">{{ advantage.title }}</p>
+                <p class="text-[#673ab7] font-semibold">{{ advantage.title }}</p>
                 <p class="text-gray-400 text-lg mt-auto">{{ advantage.description }}</p>
             </div>
         </div>
     </div>
 
-    <!-- Подробности услуги -->
-    <div class="flex flex-col gap-6">
-        <p class="mainHeading text-center text-[#673ab7]">Подробности услуги</p>
-        <div class="flex flex-col gap-2 text-gray-400 text-lg">
-            <p>{{ data[0].fullDesc }}</p>
-        </div>
+    
+    <div class="flex flex-col gap-6 items-center text-center">
+        <p class="mainHeading text-[#673ab7]">Подробности и этапы услуги</p>
+        <!-- Подробности услуги -->
+        <p class="text-gray-400 text-lg max-w-2xl">{{ data[0].fullDesc }}</p>
         
+        <!-- Этапы услуги -->
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div v-for="(stage, index) in data[0].stages" class="bg-[#252525] shadow-[0px_0px_13px_-7px_white] rounded-xl p-6 flex flex-col gap-6 items-center text-center border border-white/10">
+                <p class="text-[#673ab7] font-semibold">{{ index+1 }}. {{ stage.title }}</p>
+                <p class="text-gray-400 text-lg mt-auto">{{ stage.desc }}</p>
+            </div>
+        </div>  
+    </div>
+
+    <!-- Разделительная линия -->
+    <div class="w-full bg-white h-0.5 rounded-xl"></div>
+
+    <div class="flex flex-col items-center text-center gap-6">
+        <!-- Призыв к действию -->
+        <p class="mainHeading text-[#673ab7]">Ваши желания осуществимы</p>
+        <p class="text-gray-400 text-lg">Наши специалисты воплотили в жизнь уже сотни проектов. <br> Обратись к нам!</p>
+        <img src="/public/images/portfolio/1.jpg" alt="" class="rounded-xl aspect-video object-cover w-full lg:w-2/3">
+
         <!-- Проверка входа -->
         <div class="flex items-center gap-2 self-end" v-if="authenticated && role == 'user'">
             <button @click="startTimer" :disabled="isTimerActive" :class="{'text-white bg-[#673ab7]' : isTimerActive}" class="px-4 py-2 border border-[#673ab7] hover:text-[#673ab7] hover:bg-transparent rounded-full w-fit text-center transition-all duration-500 text-white bg-[#673ab7]">{{ timer > 0 ? `Заявка отправится через ${timer} секунд` : "Отправить заявку" }}</button>
